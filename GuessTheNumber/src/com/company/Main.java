c782fe5322ccc2cf421c187012b320c779fae0d8
Charles Saeyang <chsaeydev@gmail.com>
2020-09-22 13:10:31 -0700

    Project 2
    Guess The Number

---
+++ GuessTheNumber/src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean play = true;
+
+        while (play) {
+            play = true;
+            Random r = new Random();
+            int counter = 0;
+            int num = r.nextInt(20) + 1;
+
+            System.out.println("\nHello!, What is your name?\n");
+            String name = scan.next();
+            System.out.println("\nWell, " + name + ", I am thinking of a number between 1 and 20.");
+
+            while (true) {//Guess loop
+                System.out.println("Take a guess.\n");
+
+                String guess = scan.next();
+                int guessNum;
+                try{
+                    guessNum = Integer.parseInt(guess);
+                } catch (Exception e){
+                    System.out.println("\nInvalid! Enter a number please...");
+                    continue;
+                }
+                counter++;
+                if (guessNum > num) {
+                    System.out.println("\nYour guess is too high!");
+                } else if (guessNum < num) {
+                    System.out.println("\nYour guess is too low!");
+                } else {
+                    System.out.println("Good job, " + name + "! You guessed my number in " + counter + " guesses!");
+                    break; //break inner loop
+                }
+            }//end guess loop
+
+            while(true){//reset loop
+                System.out.println("Would you like to play again? (y or n)\n");
+                String reset = scan.next();
+                if(reset.toLowerCase().equals("n")){
+                    play = false;
+                    break;
+                }
+                else if(reset.toLowerCase().equals("y")){
+                    break;
+                }
+            }// end reset loop
+
+        }// end play loop
+
+    }
+}
