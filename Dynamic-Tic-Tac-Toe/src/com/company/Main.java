e98968afc2c14af9889f2da60cc8ff328795f7fc
Charles Saeyang <chsaeydev@gmail.com>
2020-10-14 08:36:04 -0700

    "Finished" Functional hangman.
    Still may need more functional programming

--- Dynamic-Tic-Tac-Toe/src/com/company/Main.java
+++ Dynamic-Tic-Tac-Toe/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.google.gson.Gson;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -31,7 +32,6 @@ public class Main {
         while (true) {   //Game loop
 
             if (state == GameState.Name) {//name
-
                 name = getName(scan);
                 if (!name.equals(""))
                     state = GameState.Setup;
@@ -50,13 +50,12 @@ public class Main {
                 ArrayList<String> finalWordAsList = wordAsList;
                 ArrayList<String> playBoard = wordAsList.stream().map(n -> finalGuesses.contains(n) ? n : "_").collect(Collectors.toCollection(ArrayList::new));
                 ArrayList<String> wrongBoard = guesses.stream().filter(n -> !finalWordAsList.contains(n)).collect(Collectors.toCollection(ArrayList::new));
-                if (checkWin(!playBoard.contains("_"), word) || checkLoss(wrongBoard.size(), word)) {
-                    printArt(playBoard, wrongBoard);
+                printArt(playBoard, wrongBoard);
+                if (checkWin(!playBoard.contains("_"), name, word, guesses) || checkLoss(wrongBoard.size(), word)) {
                     state = GameState.Reset;
                     continue;
                 }
 
-                printArt(playBoard, wrongBoard);
                 getInput(scan, guesses);
             }// end play state
 
@@ -77,19 +76,42 @@ public class Main {
             System.out.println("Missed letters: " + Arrays.toString(wrongBoard.toArray()));
             System.out.println(map.get(String.valueOf(wrongBoard.size())));
             System.out.println(Arrays.toString(playBoard.toArray()));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void setHighScore(String word, String name, int score, HashSet<String> guesses) {
+        Gson gson = new Gson();
+        try {
+            HashMap<String, ArrayList<String>> map = new HashMap<>();
+            map = gson.fromJson(new FileReader("C:\\Users\\chsae\\Documents\\Code\\Pyramid-Projects\\Dynamic-Tic-Tac-Toe\\src\\com\\company\\scores.json", StandardCharsets.UTF_8), map.getClass());
+            ArrayList<String> temp = new ArrayList<>() {{
+                add(name);
+                add(String.valueOf(Integer.MAX_VALUE));
+            }};
+            map.putIfAbsent(word, temp);
+            if (score <= Integer.parseInt(map.get(word).get(1))) {
+                System.out.println("New high score for word: " + "\"" + word + "\". Solved in " + guesses.size() + " guesses by " + name);
+                map.replace(word, new ArrayList<String>(Arrays.asList(name, String.valueOf(guesses.size()))));
+                String json = gson.toJson(map);
+                FileWriter writer = new FileWriter("C:\\Users\\chsae\\Documents\\Code\\Pyramid-Projects\\Dynamic-Tic-Tac-Toe\\src\\com\\company\\scores.json", StandardCharsets.UTF_8);
+                writer.write(json);
+                writer.flush();
+                writer.close();
+            } else {
+                System.out.println("Current high score: " + map.get(word).get(1) + " guesses by " + map.get(word).get(0) + ". Your score: " + guesses.size());
+            }
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void setScore(int piece) {
-
-    }
-
-    public static boolean checkWin(boolean win, String word) {
+    public static boolean checkWin(boolean win, String name, String word, HashSet<String> guesses) {
         if (win) {
             System.out.println("You Won! The word was: " + word);
+            setHighScore(word, name, guesses.size(), guesses);
             return true;
         }
         return false;
@@ -97,7 +119,7 @@ public class Main {
 
     public static boolean checkLoss(int guesses, String word) {
         if (guesses == 7) {
-            System.out.println("You lost! The word was: " + word);
+            System.out.println("You lost! The word was: " + word + "!");
             return true;
         }
         return false;
@@ -105,13 +127,12 @@ public class Main {
 
     public static String getWord() {
         Random r = new Random();
-        String x = "";
         try {
-            return Files.readAllLines(Paths.get("C:\\Users\\chsae\\Documents\\Code\\Pyramid-Projects\\Dynamic-Tic-Tac-Toe\\src\\com\\company\\WordBank")).get(r.nextInt(16));
+            return Files.readAllLines(Paths.get("C:\\Users\\chsae\\Documents\\Code\\Pyramid-Projects\\Dynamic-Tic-Tac-Toe\\src\\com\\company\\WordBank")).get(r.nextInt(15) + 1);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return x;
+        return "";
     }
 
     public static String getName(Scanner scan) {
--- TestEnvironment/src/com/company/Main.java
+++ TestEnvironment/src/com/company/Main.java
@@ -1,36 +1,36 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static com.company.Test.*;
 
 public class Main {
 
-    public enum Roles {
-        Admin,
-        User,
-        Mod,
-    }
+    public static void main(String[] args) throws IOException {
+        Charset utf8 = StandardCharsets.UTF_8;
 
-    public static void main(String[] args) {
+        //Creates a file if it does not exists -> overwrites the file by truncating and then writing
+        Files.write(Paths.get("app.log"), Arrays.asList("1","2","3","4","5"),utf8);
 
-        char c = 'A';
-        System.out.println(++c);
+        //Overloaded method. Tell it to create if it doesn't exists -> append instead of overriding
+        Files.write(Paths.get("app.log"), Arrays.asList("6","7","8"),utf8, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
 
+        Files.write(Paths.get("app.log"),"Hello World".getBytes());
 
-    }
-
-    public static String validRole(Roles role){
-        switch (role){
-            case Admin:
-                return "yo";
-            case User:
-                return "user";
-            case Mod:
-                return "loser";
-            default:
-                return "none";
-        }
+        System.out.println(Files.readAllLines(Paths.get("app.log"),utf8));
 
     }
+
+
+
 }
 
 
